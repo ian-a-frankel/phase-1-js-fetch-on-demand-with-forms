decd8a41f9def3ec95deb983b9eef6fae9e82755
Ian Frankel <ian.frankel@gmail.com>
2023-10-06 16:46:41 -0400

    movies fetcher

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const inputIdNumber = document.getElementById('searchByID').value
+        console.log(inputIdNumber)
+        fetch(`http://localhost:3000/movies/${inputIdNumber}`)
+        .then(response => response.json())
+        .then(movie => {
+            const title = document.querySelector('h4')
+            const summary = document.querySelector('p')
+            console.log(movie)
+            title.textContent = movie.title
+            summary.textContent = movie.summary
+
+
+
+        })
+
+
+    })
 }
+  
+
 
 document.addEventListener('DOMContentLoaded', init);
+
